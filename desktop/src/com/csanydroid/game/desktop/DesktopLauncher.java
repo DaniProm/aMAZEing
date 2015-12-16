9e89ce68448ab417a007995d5a838f5ef9609632
DaniProm <daniel.ruszin98@gmail.com>
2015-12-16 15:59:54 +0100

    Commit

---
+++ desktop/src/com/csanydroid/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.csanydroid.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.csanydroid.game.amazeingGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new amazeingGame(), config);
+	}
+}
