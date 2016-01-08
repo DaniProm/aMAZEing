7604bb8232ec41d007b51fcbe142ba539adc4f45
DaniProm <daniel.ruszin98@gmail.com>
2016-01-08 15:13:51 +0100

    A csillag animációja

--- core/src/com/csanydroid/game/WallActor.java
+++ core/src/com/csanydroid/game/WallActor.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 public class WallActor extends GameActor {
 
 	// H: szerintem valami másik kép kellene, mert eléggé zavaró mozgás közben
+
 	protected static Texture texture = new Texture("normwall.png");
 
 	public WallActor() {
