b63bcabb8461cf87bb563c5b3078ef5ba24f13cb
NoSpeedLimit <user.konyves@gmail.com>
2016-01-09 12:44:41 +0100

    2-4.txt

--- core/src/com/csanydroid/game/AmazingGame.java
+++ core/src/com/csanydroid/game/AmazingGame.java
@@ -16,7 +16,7 @@ public class AmazingGame extends Game implements ApplicationListener {
 	public void create () {
 
 		try {
-			setScreen(new GameScreen("1"));
+			setScreen(new GameScreen("4"));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
