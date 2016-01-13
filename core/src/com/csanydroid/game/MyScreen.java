8b49187ba9611503c419d7136debd7fb848e029c
DaniProm <daniel.ruszin98@gmail.com>
2016-01-13 19:29:23 +0100

    Képek frissítése és háttér

--- core/src/com/csanydroid/game/MyScreen.java
+++ core/src/com/csanydroid/game/MyScreen.java
@@ -120,7 +120,7 @@ public class MyScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
-		Gdx.gl.glClearColor(.7f, .7f, .7f, 1);
+		Gdx.gl.glClearColor(.0f, .08f, .0f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		batch.setProjectionMatrix(camera.combined);
